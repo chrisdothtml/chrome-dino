2ade12073a1d216c2ccffaf8991916bb5eff7cc8
Chris Deacy <chrisdothtml@gmail.com>
2018-07-21 21:34:23 -0500

    converting assets to single sprite; adding sprite image config and util

---
+++ public/lib/config.js
@@ -0,0 +1,18 @@
+export default {
+  sprites: {
+    birdUp: { h: 52, w: 84, x: 626, y: 27 },
+    birdDown: { h: 60, w: 84, x: 626, y: 79 },
+    cactus: { h: 92, w: 46, x: 68, y: 27 },
+    cactusDouble: { h: 66, w: 64, x: 114, y: 27 },
+    cactusTriple: { h: 66, w: 98, x: 178, y: 27 },
+    cloud: { h: 27, w: 92, x: 710, y: 69 },
+    dino: { h: 86, w: 80, x: 276, y: 27 },
+    dinoDuckLeftLeg: { h: 52, w: 110, x: 516, y: 27 },
+    dinoDuckRightLeg: { h: 52, w: 110, x: 516, y: 79 },
+    dinoLeftLeg: { h: 86, w: 80, x: 356, y: 27 },
+    dinoRightLeg: { h: 86, w: 80, x: 436, y: 27 },
+    gameOver: { h: 21, w: 381, x: 710, y: 48 },
+    ground: { h: 27, w: 2400, x: 0, y: 0 },
+    replayIcon: { h: 60, w: 68, x: 0, y: 27 }
+  }
+}
--- public/lib/index.js
+++ public/lib/index.js
@@ -1,26 +1,29 @@
-import { kebabToCamel } from './_utils.js'
+import config from './config.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/p5.min.js'
 
 const { p5: P5 } = window
 
 // eslint-disable-next-line no-new
 new P5(p5 => {
-  const imgs = {}
+  // eslint-disable-next-line no-unused-vars
+  let sprite
+
+  function spriteImage (spriteName, ...clientCoords) {
+    const img = config.sprites[spriteName]
+    const halfH = img.h / 2
+    const halfW = img.w / 2
+
+    // function provided for coords
+    if (clientCoords.length === 1) {
+      clientCoords = clientCoords[0]({ height: halfH, width: halfW })
+    }
+
+    // eslint-disable-next-line no-useless-call
+    return p5.image.apply(p5, [sprite, ...clientCoords, halfW, halfH, img.x, img.y, img.w, img.h])
+  }
 
   p5.preload = () => {
-    const imageNames = [
-      'bird-sprite',
-      'cactus-double',
-      'cactus-single',
-      'cactus-triple',
-      'cloud',
-      'dino-sprite',
-      'restart-icon'
-    ]
-
-    imageNames.forEach(name => {
-      imgs[kebabToCamel(name)] = p5.loadImage(`assets/${name}.png`)
-    })
+    sprite = p5.loadImage('asset-sprite.png')
   }
 
   p5.setup = () => {
@@ -28,6 +31,13 @@ new P5(p5 => {
   }
 
   p5.draw = () => {
-    //
+    p5.background('#f7f7f7')
+    spriteImage('ground', s => [ 0, (p5.height - s.height) ])
+    spriteImage('dino', s => [ 25, (p5.height - s.height - 4) ])
+    spriteImage('cloud', 350, 50)
+
+    // TODO: show when game over
+    // spriteImage('gameOver', s => [ (p5.width / 2 - s.width / 2), (p5.height / 2 - s.height / 2 - 20) ])
+    // spriteImage('replayIcon', s => [ (p5.width / 2 - s.width / 2), (p5.height / 2 - s.height / 2 + 20) ])
   }
 })
